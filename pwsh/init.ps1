155f11c0326c259bf12cc20f17534cc0ec9113a3
Freemaker <ralf.rose.oskar@gmail.com>
2024-01-15 11:11:27 +0100

    Update init.ps1

--- pwsh/init.ps1
+++ pwsh/init.ps1
@@ -1,4 +1,5 @@
 Import-Module -Name Terminal-Icons
 Import-Module -Name PSReadLine
+Import-Module "C:\Program Files\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
 
 oh-my-posh init pwsh --config "$env:USERCONFIG/pwsh/oh-my-posh.theme.json" | Invoke-Expression

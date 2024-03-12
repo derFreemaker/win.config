dfd2e567bd05637d0ddb258042c0e83549d4bfe6
Freemaker <ralf.rose.oskar@gmail.com>
2024-03-12 16:11:54 +0100

    sync

--- pwsh/init.ps1
+++ pwsh/init.ps1
@@ -32,7 +32,12 @@ Import-Module -Name Terminal-Icons
 Import-Module -Name PSReadLine
 Import-Module "C:\Program Files\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
 
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if ($null -eq $env:ChocolateyInstall) {
+    Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+}
+else {
+    . "$env:USERCONFIG_FREEMAKER/tools/Update-SessionEnvironment.ps1"
+}
 
 # oh-my-posh init pwsh --config "$env:USERCONFIG_FREEMAKER/pwsh/oh-my-posh.theme.json" | Invoke-Expression
 

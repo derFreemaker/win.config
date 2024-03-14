8c58832198ee13465cd37fe8528d8880b5706781
Freemaker <ralf.rose.oskar@gmail.com>
2024-03-14 11:10:39 +0100

    sync

--- pwsh/init.ps1
+++ pwsh/init.ps1
@@ -3,7 +3,8 @@ if ($Global:USERCONFIG_FREEMAKER_INIT -eq 1) {
 }
 
 if ($null -ne $env:USERCONFIG_FREEMAKER_PORTABLE) {
-    . "$PSScriptRoot/../portable/cleanup.ps1"
+    Write-Warning "portable modus is still active!"
+    Write-Warning "This is NOT the portable terminal."
 }
 
 . "$PSScriptRoot/core_init.ps1"
--- pwsh/portable/init.ps1
+++ pwsh/portable/init.ps1
@@ -1,4 +1,4 @@
-if ($Global:USERCONFIG_FREEMAKER_PORTABLE_INIT -eq 1) {
+if ($Global:USERCONFIG_FREEMAKER_INIT -eq 1) {
     exit
 }
 
@@ -7,11 +7,11 @@ if ($Global:USERCONFIG_FREEMAKER_PORTABLE_INIT -eq 1) {
 if ($null -eq $env:USERCONFIG_FREEMAKER_PORTABLE) {
     . "$PSScriptRoot/../../portable/setup.ps1"
 }
-$env:USERCONFIG_FREEMAKER = $env:USERCONFIG_FREEMAKER_PORTABLE
 
+$env:USERCONFIG_FREEMAKER = $env:USERCONFIG_FREEMAKER_PORTABLE
 $env:ChocolateyInstall = Get-ENV -VariableName "ChocolateyInstall"
 $env:ChocolateyToolsLocation = Get-ENV -VariableName "ChocolateyToolsLocation"
 
 . "$PSScriptRoot/../core_init.ps1"
 
-$Global:USERCONFIG_FREEMAKER_PORTABLE_INIT = 1
+$Global:USERCONFIG_FREEMAKER_INIT = 1

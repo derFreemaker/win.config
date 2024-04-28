eac8bc82f249df980860f61ea5b7e13a2460a45c
Freemaker <ralf.rose.oskar@gmail.com>
2024-04-28 22:03:41 +0200

    added some other programs

--- tools/install.ps1
+++ tools/install.ps1
@@ -6,3 +6,15 @@ Invoke-Expression 'choco install cmake.install cmake --installargs "ADD_CMAKE_TO
 # can be removed when path gets added with install
 Add-ENV -VariableName "Path" -Value "C:\Program Files\CMake\bin"
 $env:PATH = "$env:PATH;C:\Program Files\CMake\bin"
+
+# Git & co
+Invoke-Expression winget install --id Git.Git
+Invoke-Expression winget install --id GitHub.GitHubDesktop
+Invoke-Expression winget install --id Axosoft.GitKraken
+Invoke-Expression winget install --id GitHub.cli
+
+# Brave
+Invoke-Expression winget install --id Brave.Brave
+
+# JetBrains
+Invoke-Expression winget install --id JetBrains.Toolbox

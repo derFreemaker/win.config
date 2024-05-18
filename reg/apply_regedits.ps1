606edeef694f047b0f9aafe27be55adce71cd9d3
Freemaker <ralf.rose.oskar@gmail.com>
2024-05-18 23:15:56 +0200

    added registry script to diactivate bing search results in search bar in the task bar

--- reg/apply_regedits.ps1
+++ reg/apply_regedits.ps1
@@ -3,4 +3,7 @@ Write-Output "applying registry edits..."
 Write-Output "remove shortcut arrow..."
 Invoke-Expression "sudo reg import $PSScriptRoot\remove_shortcut_arrow_icon.reg"
 
+Write-Output "disabling search bing results..."
+Invoke-Expression "sudo reg import $PSScriptRoot\disable_bin_search_results.reg"
+
 Write-Output "applyed registry edits!"
--- reg/remove_regedits.ps1
+++ reg/remove_regedits.ps1
@@ -1,6 +1,9 @@
 Write-Output "remove registry edits..."
 
-Write-Output "remove 'remove shortcut arrow'..."
-Invoke-Expression "sudo reg import $PSScriptRoot\remove_remove_shortcut_arrow_icon.reg"
+Write-Output "adding shortcut arrow..."
+Invoke-Expression "sudo reg import $PSScriptRoot\add_shortcut_arrow_icon.reg"
+
+Write-Output "enabling bing search results..."
+Invoke-Expression "sudo reg import $PSScriptRoot\enable_bin_search_results.reg"
 
 Write-Output "removed registry edits!"

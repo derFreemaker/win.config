9b3995d402dda9c43756ab6b0f6ef9db3454ee94
Freemaker <ralf.rose.oskar@gmail.com>
2024-06-10 19:54:11 +0200

    sync

--- pwsh/init.ps1
+++ pwsh/init.ps1
@@ -6,10 +6,12 @@ if ($null -ne $env:USERCONFIG_FREEMAKER_PORTABLE) {
     if (!(Test-Path -Path $env:DRIVE_FREEMAKER_PORTABLE)) {
         . "$PSScriptRoot/../portable/cleanup.ps1"
     }
+}
 
+if ($null -ne $env:USERCONFIG_FREEMAKER_PORTABLE) {
     Write-Warning "portable mode is active"
     . "$PSScriptRoot/portable/init.ps1"
-} else {    
+} else {
     . "$PSScriptRoot/core_init.ps1"
 }
 

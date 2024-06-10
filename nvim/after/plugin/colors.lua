20f605df60a23b7d2d035cd8925bb21981b66459
Freemaker <ralf.rose.oskar@gmail.com>
2024-06-10 23:59:18 +0200

    fixed some stuff

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,8 +1,8 @@
 function LoadColorScheme(color)
-	color = color or "tokyonight-storme"
+	color = color or "tokyonight-storm"
 	vim.cmd.colorscheme(color)
 
-evim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 

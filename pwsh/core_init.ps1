1fde03c83bcda55c0bf0921d9ff997c10e39f25d
Freemaker <ralf.rose.oskar@gmail.com>
2024-10-16 14:20:14 +0200

    added brave as alias

--- pwsh/core_init.ps1
+++ pwsh/core_init.ps1
@@ -1,37 +1,33 @@
-function Get-GitStatus { & git status -sb $args }
-function Get-GitCommit { & git commit -ev $args }
-function Get-GitAdd { & git add --all $args }
-function Get-GitTree { & git log --graph --oneline --decorate $args }
-function Get-GitPush { & git push $args }
-function Get-GitPull { & git pull --rebase $args }
-function Get-GitFetch { & git fetch $args }
-function Get-GitCheckout { & git checkout $args }
-function Get-GitBranch { & git branch $args }
-function Get-GitRemote { & git remote -v $args }
-function Update-GitSubmodules { & git submodule update --remote --recursive $args }
-function Restore-Git { & git restore $args }
-function Git-Cherry-Pick { & git cherry-pick $args }
-New-Alias -Name gs -Value Get-GitStatus -Force
-New-Alias -Name gc -Value Get-GitCommit -Force
-New-Alias -Name ga -Value Get-GitAdd -Force
-New-Alias -Name gt -Value Get-GitTree -Force
-New-Alias -Name gps -Value Get-GitPush -Force
-New-Alias -Name gpl -Value Get-GitPull -Force
-New-Alias -Name gf -Value Get-GitFetch -Force
-New-Alias -Name gco -Value Get-GitCheckout -Force
-New-Alias -Name gb -Value Get-GitBranch -Force
-New-Alias -Name gr -Value Get-GitRemote -Force
-New-Alias -Name gsmu -Value Update-GitSubmodules -Force
-New-Alias -Name grst -Value Restore-Git -Force
-New-Alias -Name gcp -Value Git-Cherry-Pick -Force
+# Browser
+New-Alias -Name brave -Value "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" -Force
 
-# bazel
-function Bazel-Build { & bazel build $args }
-function Bazel-Run { & bazel run $args }
-function Bazel-Clean { & bazel clean $args }
-New-Alias -Name bbuild -Value Bazel-Build -Force
-New-Alias -Name brun -Value Bazel-Run -Force
-New-Alias -Name bclean -Value Bazel-Clean -Force
+# Git
+function Get-GitStatus { & git status -sb $args }
+New-Alias -Name gs -Value Get-GitStatus -Force
+function Get-GitCommit { & git commit -ev $args }
+New-Alias -Name gc -Value Get-GitCommit -Force
+function Get-GitAdd { & git add --all $args }
+New-Alias -Name ga -Value Get-GitAdd -Force
+function Get-GitTree { & git log --graph --oneline --decorate $args }
+New-Alias -Name gt -Value Get-GitTree -Force
+function Get-GitPush { & git push $args }
+New-Alias -Name gps -Value Get-GitPush -Force
+function Get-GitPull { & git pull --rebase $args }
+New-Alias -Name gpl -Value Get-GitPull -Force
+function Get-GitFetch { & git fetch $args }
+New-Alias -Name gf -Value Get-GitFetch -Force
+function Get-GitCheckout { & git checkout $args }
+New-Alias -Name gco -Value Get-GitCheckout -Force
+function Get-GitBranch { & git branch $args }
+New-Alias -Name gb -Value Get-GitBranch -Force
+function Get-GitRemote { & git remote -v $args }
+New-Alias -Name gr -Value Get-GitRemote -Force
+function Update-GitSubmodules { & git submodule update --remote --recursive $args }
+New-Alias -Name gsmu -Value Update-GitSubmodules -Force
+function Restore-Git { & git restore $args }
+New-Alias -Name grst -Value Restore-Git -Force
+function Git-Cherry-Pick { & git cherry-pick $args }
+New-Alias -Name gcp -Value Git-Cherry-Pick -Force
 
 # Neovim
 New-Alias -Name vim -Value nvim -Force

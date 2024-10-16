20c66e0d9e2422f642b31f9809a006bed49fa253
Freemaker <ralf.rose.oskar@gmail.com>
2024-10-16 17:05:40 +0200

    updated chatgpt powertoys plugin script

--- powertoys/plugins/chatgpt/install.ps1
+++ powertoys/plugins/chatgpt/install.ps1
@@ -2,4 +2,4 @@ Write-Host "installing powertoys plugin for chatgpt..."
 
 $temp = New-TemporaryFile
 Invoke-WebRequest -Uri "https://github.com/ferraridavide/ChatGPTPowerToys/releases/download/v0.85.1/Community.PowerToys.Run.Plugin.ChatGPT.x64.zip" -OutFile $temp
-Invoke-Expression "7z e $temp -o'C:\Program Files\PowerToys\RunPlugins\ChatGPT'"
+Invoke-Expression "7z e $temp -o'$env:LOCALAPPDATA/Microsoft/PowerToys/PowerToys Run/Plugins/ChatGPT'"

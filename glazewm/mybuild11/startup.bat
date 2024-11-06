08de4bbac78a04fd60f8a915112fecacaf7ffff0
Freemaker <ralf.rose.oskar@gmail.com>
2024-11-06 21:54:04 +0100

    now launching gg steel series sonar with glazewm

---
+++ glazewm/mybuild11/startup.bat
@@ -0,0 +1,1 @@
+start pwsh %USERCONFIG_FREEMAKER%/glazewm/mybuild11/startup.ps1 --no-profile
---
+++ glazewm/mybuild11/startup.ps1
@@ -0,0 +1,1 @@
+start "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\SteelSeries\SteelSeries GG\SteelSeries GG.lnk"

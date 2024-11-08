f5f6422fa5a388bb64f71ca65d5c35a491c5d247
Freemaker <ralf.rose.oskar@gmail.com>
2024-11-08 09:58:15 +0100

    add glazewm startup to setup script

--- glazewm/portable/setup.ps1
+++ glazewm/portable/setup.ps1
@@ -2,3 +2,5 @@ Write-Output "copying window manager config..."
 New-Item -ItemType Directory -Path "$env:USERPROFILE/.glzr" -Force
 New-Item -ItemType Directory -Path "$env:USERPROFILE/.glzr/glazewm" -Force
 Copy-Item -Path "$env:USERCONFIG_FREEMAKER/glazewm/config.yaml" -Destination "$env:USERPROFILE/.glzr/glazewm/config.yaml" -Force
+
+Start-Process -FilePath "$env:DRIVE_FREEMAKER_PORTABLE/Tools/GlazeWM/glazewm.exe"

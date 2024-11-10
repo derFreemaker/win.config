b258153d622719676cc397f66151745733c41f80
Freemaker <ralf.rose.oskar@gmail.com>
2024-11-10 20:38:49 +0100

    remove install_new.ps1

--- install_new.ps1
+++
@@ -1,5 +0,0 @@
-$driveLetter = $pwd.drive.name + ":"
-$Global:DriveLetter = $driveLetter
-
-$env:PATH = "$PSScriptRoot/lua/;" + $env:PATH
-Invoke-Expression "$PSScriptRoot/lua-config/bin/lua-config.bat device --install"

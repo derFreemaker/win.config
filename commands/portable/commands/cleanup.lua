a2c791de0a14d1613f6241a14a577f06d2ffb1b4
Freemaker <ralf.rose.oskar@gmail.com>
2024-11-23 10:30:51 +0100

    fix cleanup script

--- commands/portable/commands/cleanup.lua
+++ commands/portable/commands/cleanup.lua
@@ -12,44 +12,52 @@ if not lfs.exists(tools_dir) or lfs.attributes(tools_dir).mode ~= "directory" th
 end
 cleanup_throbber:rotate()
 
-for tool in lfs.dir(tools_dir) do
+local function cleanup_tool(tool)
     local tool_path = tools_dir .. tool
+    local tool_seg = terminal:print("cleaning up tool '" .. tool .. "'")
+
     local attr = lfs.attributes(tool_path)
     if not attr or attr.mode ~= "directory" or tool == "." or tool == ".." then
-        goto continue
+        return
     end
     cleanup_throbber:rotate()
 
     local tool_config_path = tool_path .. "/.config/"
     if not lfs.exists(tool_config_path) then
-        goto continue
+        return
     end
-    cleanup_throbber:rotate()
 
     local disable_path = tool_config_path .. ".disable"
     if lfs.exists(disable_path) then
         print("tool '" .. tool .. "' is disabled")
-        goto continue
+        return
     end
     cleanup_throbber:rotate()
 
     local tool_cleanup_path = tool_config_path .. "cleanup.lua"
     if not lfs.exists(tool_cleanup_path) then
-        goto continue
+        return
     end
-    cleanup_throbber:rotate()
 
     local cleanup_func, err_msg = loadfile(tool_cleanup_path)
     if not cleanup_func then
         print("unable to load cleanup file for tool '" .. tool .. "'\n" .. err_msg)
-        goto continue
+        return
     end
     cleanup_throbber:rotate()
 
-    print("cleaning up tool '" .. tool .. "'")
-    cleanup_func()
+    local tool_thread = coroutine.create(cleanup_func)
+    local success, setup_err_msg = coroutine.resume(tool_thread)
+    tool_seg:remove()
+    if not success then
+        print("tool '" .. tool .. "' cleanup failed with:\n"
+            .. debug.traceback(tool_thread, setup_err_msg))
+    end
+    coroutine.close(tool_thread)
+end
 
-    ::continue::
+for tool in lfs.dir(tools_dir) do
+    cleanup_tool(tool)
     cleanup_throbber:rotate()
 end
 
